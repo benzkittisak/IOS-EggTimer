1f73c1a12588aed75be06e27a6e7038de1818919
benzkittisak <benzkittisak@stv.ac.th>
2022-06-22 11:01:38 +0700

    update comment

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -9,9 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    let eggTimes = ["Soft": 300 , "Medium" : 420 , "Hard" : 720]
+    let eggTimes = ["Soft": 3 , "Medium" : 420 , "Hard" : 720]
     
     var secondsRemaining = 60
+    
+//    แก้ปัญหากดปุ่มหลาย ๆ ทีแล้วเวลามันแปลกๆด้วยการสร้างตัวแปรซะเลย
+    var timer = Timer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,11 +24,16 @@ class ViewController: UIViewController {
     
     
     @IBAction func hardnessSelection(_ sender: UIButton) {
+        
+//        สั่งให้มันลบหรือยกเลิกการลูปนับเวลาออกจากกัน
+        timer.invalidate()
+        
         guard let hardness = sender.currentTitle else { return print("Error Title is nil !!") }
         
         secondsRemaining = eggTimes[hardness]!
         
-        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+//        ย้ายคำสั่งนับเวลาถอยหลังมาเก็บไว้ในตัวแปร
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
     }
     
     
