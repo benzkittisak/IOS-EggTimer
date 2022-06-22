c521fe6a2d2a0ec0a146a0bc4fd004126f614d5b
benzkittisak <benzkittisak@stv.ac.th>
2022-06-22 10:58:45 +0700

    update code

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let eggTimes = ["Soft": 300 , "Medium" : 420 , "Hard" : 720]
+    
+    var secondsRemaining = 60
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        print(eggTimes)
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func hardnessSelection(_ sender: UIButton) {
+        guard let hardness = sender.currentTitle else { return print("Error Title is nil !!") }
+        
+        secondsRemaining = eggTimes[hardness]!
+        
+        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
+    
+    
+    @objc func updateTimer() {
+        if secondsRemaining > 0 {
+            print("\(secondsRemaining) seconds.")
+            secondsRemaining -= 1
+        }
+    }
 }
 
